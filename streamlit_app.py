7bf4dcc4bd3b1dd6a3d4ff46be513edbf4950003
lmservian <93549435+lmservian@users.noreply.github.com>
2022-08-31 15:13:22 +1200

    Update streamlit_app.py
    add try-catch.

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,19 +36,25 @@ streamlit.dataframe(fruits_to_show)
 # Fruityvice Fruit Advice
 #
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+#   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+      streamlit.error("Please, select a fruit to get information.")
+   else:
+      #streamlit.write('The user entered ', fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+   streamlit.error()
 
 
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())
-
-
 # transform the json response into a pandas dataframe
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # adds the dataframe to the app
-streamlit.dataframe(fruityvice_normalized)
+
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
