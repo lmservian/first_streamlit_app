62ab8c072e6b300e3f178d3e78deacb15b8a51e3
lmservian <93549435+lmservian@users.noreply.github.com>
2022-08-31 15:23:35 +1200

    Update streamlit_app.py
    adding a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,28 +32,32 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+def get_fruitvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+
 #
 # Fruityvice Fruit Advice
 #
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-#   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
       streamlit.error("Please, select a fruit to get information.")
    else:
-      #streamlit.write('The user entered ', fruit_choice)
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruitvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
    streamlit.error()
 
-
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice)
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json())
-# transform the json response into a pandas dataframe
-# adds the dataframe to the app
+#transform the json response into a pandas dataframe
+#adds the dataframe to the app
 
 
 streamlit.stop()
