ac42f41b3bb139f123b88d2f0ff5c805d882c740
lmservian <93549435+lmservian@users.noreply.github.com>
2022-08-31 12:55:26 +1200

    Update streamlit_app.py
    reversing changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import pandas
-#import requests
+import requests
 import streamlit
 
 streamlit.text(fruityvice_response)
@@ -29,8 +29,3 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
-
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
